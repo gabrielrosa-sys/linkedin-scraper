663f4f7584ae8b6797093cad2a2c545151b59c8c
gabrielrosa-sys <gabrielrosa@cedae.com.br>
2026-04-16 17:45:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.get('/linkedin', async (req, res) => {
   }
 });
 
-app.listen(10000, () => {
-  console.log('Servidor rodando');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' + PORT);
 });
